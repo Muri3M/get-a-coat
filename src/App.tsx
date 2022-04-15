dba9a258d00dd35fd36bfd6ff581d40b9b7aafe4
Murilo Mourtada <murilo.mourtada@unifesp.br>
2022-04-14 21:57:12 -0300

    feat: create temperature component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Container, Card } from '@mui/material';
+import Temperature from './components/Temperature';
+import Input from './components/Input';
 
 function App() {
-  return <div className="App"> oi</div>;
+  return (
+    <Container
+      disableGutters
+      sx={{
+        bgcolor: '#0D1B2A',
+        height: '100vh',
+        maxWidth: '100vw !important',
+
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Card
+        sx={{
+          bgcolor: '#415A77',
+          p: 4,
+          width: '60%',
+          height: '40%',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Input />
+        <Temperature />
+      </Card>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Temperature.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { Card, Typography } from '@mui/material';
+
+import { shallowEqual, useSelector } from 'react-redux';
+import TEMPERATURE from '../querys';
+
+interface getTemperatureData {
+  getCityByName: {
+    weather: {
+      temperature: {
+        actual: number;
+      };
+    };
+  };
+}
+
+function Temperature() {
+  const name: string = useSelector(
+    (state: CityState) => state.city,
+    shallowEqual
+  );
+
+  const { loading, error, data } = useQuery<getTemperatureData>(TEMPERATURE, {
+    variables: { name },
+  });
+
+  const temperature = data?.getCityByName?.weather?.temperature?.actual;
+
+  if (name === '') {
+    return <div />;
+  }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return (
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        gutterBottom
+        color="#f53b3b"
+        component="div"
+      >
+        Acontecu um erro
+      </Typography>
+    );
+  }
+
+  if (temperature === undefined) {
+    return (
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        gutterBottom
+        color="#E0E1DD"
+        component="div"
+      >
+        Cidade não encontrada
+      </Typography>
+    );
+  }
+
+  return (
+    <Card
+      sx={{
+        bgcolor: '#415A77',
+        boxShadow: 'none',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Typography
+        variant="h2"
+        fontWeight="bold"
+        gutterBottom
+        color={temperature > 293 ? '#DE6E4B' : '#778da9'}
+        component="div"
+      >
+        {(temperature - 273.15).toFixed(2)}°
+      </Typography>
+      <Typography
+        variant="h5"
+        fontWeight="bold"
+        gutterBottom
+        color="#E0E1DD"
+        component="div"
+      >
+        {temperature > 293
+          ? `Em ${name} esta quente não precisa de casaco`
+          : `Em ${name} esta frio é melhor levar o casaco`}
+      </Typography>
+    </Card>
+  );
+}
+
+export default Temperature;
